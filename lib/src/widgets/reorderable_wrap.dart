4becf82d752a901cc25f881566f76658f1732f76
Kittipong Theothaisong <kittipong.knos@gmail.com>
2022-05-25 18:38:17 +0700

    fixed width of drag target marker

--- lib/src/widgets/reorderable_wrap.dart
+++ lib/src/widgets/reorderable_wrap.dart
@@ -1146,21 +1146,21 @@ class _ReorderableWrapContentState extends State<_ReorderableWrapContent>
             Positioned(
               left: 0,
               top: 0,
-              width: _childSizes[index].width,
+              width: MediaQuery.of(context).size.width,
               height: _childSizes[index].height * 0.5,
               child: preDragTarget,
             ),
             Positioned(
               right: 0,
               bottom: 0,
-              width: _childSizes[index].width,
+              width: MediaQuery.of(context).size.width,
               height: _childSizes[index].height * 0.5,
               child: nextDragTarget,
             ),
             Positioned(
               right: 0,
               bottom: 0,
-              width: _childSizes[index].width,
+              width: MediaQuery.of(context).size.width,
               height: _childSizes[index].height,
               child: Center(
                 child: Text(
